ba68472e2d39e06b2cade607c8ede61ea96d352d
ayeV <36230640+ayeV@users.noreply.github.com>
2018-04-11 10:30:09 -0300

    Add files via upload

---
+++ cadenaCaracteres/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define CANT 2
+int main()
+{   int legajo[CANT], sueldo[CANT];
+    char nombre[CANT][31];
+    int i;
+    for(i=0; i<CANT; i++) {
+
+        legajo[i]=i+1;
+        printf("Nombre: ");
+        fflush(stdin);
+        scanf("%[^\n]", nombre[i]);
+        printf("Sueldo: ");
+        scanf("%d", &sueldo[i]);
+    }
+    printf("\nLeg\tNombre\tSueldo");
+
+    for(i= 0; i<CANT; i++){
+            printf("\n%d\t%s\t%d", legajo[i], nombre[i], sueldo[i]);
+    }
+
+    return 0;
+}
